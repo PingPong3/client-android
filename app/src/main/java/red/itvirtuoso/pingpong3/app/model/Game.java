f83fbe2d8156fbfe90df8847dad5c341d972cc31
NAKAGAKI Kenji <nakaken0629@gmail.com>
2015-03-13 22:59:44 +0900

    junit4で単体テストができるようにした

---
+++ app/src/main/java/red/itvirtuoso/pingpong3/app/model/Game.java
@@ -0,0 +1,7 @@
+package red.itvirtuoso.pingpong3.app.model;
+
+/**
+ * Created by kenji on 15/03/12.
+ */
+public class Game {
+}
---
+++ app/src/test/java/red/itvirtuoso/pingpong3/app/model/GameTest.java
@@ -0,0 +1,16 @@
+package red.itvirtuoso.pingpong3.app.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+/**
+ * Created by kenji on 15/03/13.
+ */
+public class GameTest {
+
+    @Test
+    public void ゲームを開始する() throws Exception {
+        fail("テストを作成していない");
+    }
+}
