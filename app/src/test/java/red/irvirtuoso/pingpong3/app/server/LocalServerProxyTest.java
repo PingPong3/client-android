f4714713372a0cd2eafcbf77d8cfbfaeaa532b34
NAKAGAKI Kenji <nakaken0629@gmail.com>
2015-05-03 10:28:31 +0900

    ローカルサーバでリターンを行った時のテストコードを作成

--- app/src/test/java/red/irvirtuoso/pingpong3/app/server/LocalServerProxyTest.java
+++ app/src/test/java/red/irvirtuoso/pingpong3/app/server/LocalServerProxyTest.java
@@ -94,6 +94,7 @@ public class LocalServerProxyTest {
         List<_Log> logs = new ArrayList<>();
         server.send(new Packet(PacketType.SWING));
         while (logs.size() < 6) {
+            Thread.yield();
             Packet packet = server.receive();
             if (packet == null) {
                 continue;
@@ -108,6 +109,50 @@ public class LocalServerProxyTest {
                 builder.create(3, PacketType.RETURN),
                 builder.create(5, PacketType.BOUND_MY_AREA),
                 builder.create(7, PacketType.POINT_RIVAL)
+        )));
+    }
+
+    @Test(timeout = 1000)
+    public void リターンをする() throws Exception {
+        /*
+         * ボールが返ってきたときにSWINGパケットを送信すると、次のパケットを順に受信する
+         * <ul>
+         *     <li>6, リターン</li>
+         *     <li>8, 相手陣でのバウンド</li>
+         *     <li>9, 相手のリターン</li>
+         *     <li>11, 自陣でのバウンド</li>
+         *     <li>13, 相手の得点</li>
+         * </ul>
+         */
+        LocalServerProxy server = new LocalServerProxy(STEP_TIME);
+        _LogBuilder builder = new _LogBuilder(STEP_TIME);
+        List<_Log> tempLogs = new ArrayList<>();
+        server.send(new Packet(PacketType.SWING));
+        while (tempLogs.size() < 5) {
+            Thread.yield();
+            Packet packet = server.receive();
+            if (packet == null) {
+                continue;
+            }
+            tempLogs.add(builder.create(packet.getType()));
+        }
+        List<_Log> logs = new ArrayList<>();
+        server.send(new Packet(PacketType.SWING));
+        while (logs.size() < 5) {
+            Thread.yield();
+            Packet packet = server.receive();
+            if (packet == null) {
+                continue;
+            }
+            logs.add(builder.create(packet.getType()));
+        }
+
+        assertThat(logs, is(contains(
+                builder.create(6, PacketType.RETURN),
+                builder.create(8, PacketType.BOUND_RIVAL_AREA),
+                builder.create(9, PacketType.RETURN),
+                builder.create(11, PacketType.BOUND_MY_AREA),
+                builder.create(13, PacketType.POINT_RIVAL)
         )));
     }
 }
